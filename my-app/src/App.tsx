be6314376b795d681fcb26c6d85e076a210ad23b
Rax <peter030910@gmail.com>
2022-10-19 13:54:17 +0800

    init

---
+++ my-app/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { ReactComponent as Circle } from './images/circle.svg'
+import './App.css';
+import Menu from './components/Menu/Menu';
+
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-Header"></div>
+    </div>
+  );
+}
+
+export default App;
