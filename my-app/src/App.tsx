ea30f00310e51c1c1571e336f0404ebbc35c9427
Rax <peter030910@gmail.com>
2022-10-19 14:32:56 +0800

    feat: :art: Add Menu Button

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { ReactComponent as Circle } from './images/circle.svg'
 import './App.css';
-
+import Menu from './components/Menu/Menu';
 
 function App() {
   return (
     <div className="App">
-      <div className="App-Header"></div>
+      <div className="App-Header">
+        <Menu />
+      </div>
     </div>
   );
 }
---
+++ my-app/src/components/Menu/Menu.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import {ReactComponent as  ImageMenu} from '../../images/menu.svg'
+function Menu() {
+  return (
+    <div>
+        <ImageMenu height={30} width={30} stroke="black"/>
+    </div>
+  )
+}
+
+export default Menu
