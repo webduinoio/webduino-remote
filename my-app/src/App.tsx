8e0d72e0982ef86ad85bd6de3a96dcd32efc855e
Rax <peter030910@gmail.com>
2022-10-19 17:28:16 +0800

    feat: :art: Add hamburger button

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { ReactComponent as Circle } from './images/circle.svg'
 import './App.css';
+import Hamburger from './src/Hamburger/Hamburger';
 
 
 function App() {
   return (
     <div className="App">
-      <div className="App-Header"></div>
+      <div className="App-Header">
+        <Hamburger />
+      </div>
     </div>
   );
 }
---
+++ my-app/src/src/Hamburger/Hamburger.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import {ReactComponent as ImageHamburger} from '../../images/hamburger.svg'
+function Hamburger() {
+  return (
+    <div>
+        <ImageHamburger height={30} width={30}/>
+    </div>
+  )
+}
+
+export default Hamburger
